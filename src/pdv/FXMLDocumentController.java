9eb6d6f75c8550efe7de31063ff46acd4a22e7a5
Vinicius-rodrigues <viniciusrodrigues93@yahoo.com.br>
2018-02-15 19:01:02 -0200

    Primeiro commit

---
+++ src/pdv/FXMLDocumentController.java
@@ -0,0 +1,30 @@
+package pdv;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+/**
+ *
+ * @author vinicius
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Menu menuCadastro; 
+    @FXML
+    private MenuItem menuCadastrosCliente;
+    @FXML
+    private MenuItem menuCadastrosProdutos;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+
+}
